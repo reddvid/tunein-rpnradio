be385666e492779576b6dc077b6e923cd58e8ab5
David Ballesteros <david_ballesteros@outlook.com>
2021-04-09 03:12:47 +0800

    Fix url to be compatible with subfolder

--- script.js
+++ script.js
@@ -2,110 +2,108 @@ function itemClick(e) {
   //e.preventDefault();
 
   //var elm = e.target;
-  var audio = document.getElementById('audio');
+  var audio = document.getElementById("audio");
 
-  var source = document.getElementById('audioSource');
-  source.src = "http://87.98.130.255:" + e.getAttribute('port') + "/stream?type=http&amp;nocache=355";
+  var source = document.getElementById("audioSource");
+  source.src =
+    "http://87.98.130.255:" +
+    e.getAttribute("port") +
+    "/stream?type=http&amp;nocache=355";
 
-  var st = e.getAttribute('x').toUpperCase();
+  var st = e.getAttribute("x").toUpperCase();
   audio.load(); //call this to just preload the audio without playing
   //audio.play(); //call this to play the song right away
-  audio.play()
-    .then(_ => {
-
-      if ('mediaSession' in navigator) {
-
+  audio
+    .play()
+    .then((_) => {
+      if ("mediaSession" in navigator) {
         navigator.mediaSession.metadata = new MediaMetadata({
-          title: 'RPN ' + st,
-          artist: 'RPN Radio',
-          artwork: [{
-            src: 'http://rpnradio.com/tunein/images/thumbnails/ronda-96.png',
-            sizes: '96x96',
-            type: 'image/png'
-          },
-          {
-            src: 'http://rpnradio.com/tunein/images/thumbnails/ronda-128.png',
-            sizes: '128x128',
-            type: 'image/png'
-          },
-          {
-            src: 'http://rpnradio.com/tunein/images/thumbnails/ronda-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'http://rpnradio.com/tunein/images/thumbnails/ronda-256.png',
-            sizes: '256x256',
-            type: 'image/png'
-          },
-          {
-            src: 'http://rpnradio.com/tunein/images/thumbnails/ronda-384.png',
-            sizes: '384x384',
-            type: 'image/png'
-          },
-          {
-            src: 'http://rpnradio.com/tunein/images/thumbnails/ronda-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          ]
+          title: "RPN " + st,
+          artist: "RPN Radio",
+          artwork: [
+            {
+              src: "http://rpnradio.com/tuneinimages/thumbnails/ronda-96.png",
+              sizes: "96x96",
+              type: "image/png",
+            },
+            {
+              src: "http://rpnradio.com/tuneinimages/thumbnails/ronda-128.png",
+              sizes: "128x128",
+              type: "image/png",
+            },
+            {
+              src: "http://rpnradio.com/tuneinimages/thumbnails/ronda-192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "http://rpnradio.com/tuneinimages/thumbnails/ronda-256.png",
+              sizes: "256x256",
+              type: "image/png",
+            },
+            {
+              src: "http://rpnradio.com/tuneinimages/thumbnails/ronda-384.png",
+              sizes: "384x384",
+              type: "image/png",
+            },
+            {
+              src: "http://rpnradio.com/tuneinimages/thumbnails/ronda-512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
         });
 
-        navigator.mediaSession.setActionHandler('play', function () {
+        navigator.mediaSession.setActionHandler("play", function () {
           audio.play();
         });
-        navigator.mediaSession.setActionHandler('pause', function () {
+        navigator.mediaSession.setActionHandler("pause", function () {
           audio.stop();
         });
       }
-
     })
-    .catch(error => {
-      console.log(error)
+    .catch((error) => {
+      console.log(error);
     });
 
   //Get and Set Image Url String
-  var urlString = 'url(/images/' + e.getAttribute('x') + '.jpg';
+  var urlString = "url(images/" + e.getAttribute("x") + ".jpg";
 
   //Station Cover Photo
-  var imgcontainer = document.getElementById('img-container');
+  var imgcontainer = document.getElementById("img-container");
   imgcontainer.style.backgroundImage = urlString;
-  imgcontainer.style.height = '30vh';
-  imgcontainer.style.width = '768px';
+  imgcontainer.style.height = "30vh";
+  imgcontainer.style.width = "768px";
   imgcontainer.style.display = "block";
 
   //Show Audio Controls
-  var player = document.getElementById('player');
+  var player = document.getElementById("player");
   player.style.display = "table";
-
-
-};
+}
 
 var isNavOpen = false;
 
 //Navigations
 function closeNav() {
-  document.getElementById('mobile-nav').classList.remove('show');
+  document.getElementById("mobile-nav").classList.remove("show");
   isNavOpen = false;
-};
+}
 
 function openNav() {
-  document.getElementById('mobile-nav').classList.add('show');
-  document.getElementById('mobile-nav').classList.add('show');
+  document.getElementById("mobile-nav").classList.add("show");
+  document.getElementById("mobile-nav").classList.add("show");
   isNavOpen = true;
-};
+}
 
 function goBack() {
   if (isNavOpen) {
     closeNav();
-  }
-  else if (document.referrer == "") {
+  } else if (document.referrer == "") {
     window.location.href = "https://tunein.rpnradio.com/";
+  } else {
+    window.history.back();
   }
-  else {
-    window.history.back()
-  }
-};
+}
 
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
@@ -120,13 +118,17 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
-          if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      }
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       /* Exit the function: */
